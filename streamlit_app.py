79a2abf6743b0a90a093b5990d5c281d152822f1
robertjohnmartin <64300390+robertjohnmartin@users.noreply.github.com>
2024-12-18 11:00:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    pd_df,
+    my_dataframe,
     max_selections=5
 )
 
@@ -43,16 +43,16 @@ if ingredients_list:
     
     ingredients_string = ''
     
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        #st.text(smoothiefroot_response.json())
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        for fruit_chosen in ingredients_list:
+            ingredients_string += fruit_chosen + ' '
+            
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+            
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+            #st.text(smoothiefroot_response.json())
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
